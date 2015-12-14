07f880a9016facc1715ea0b7a70a56e0b7056a67
iSosnitsky <spiritblossom@icloud.com>
2015-12-14 23:37:34 +0500

    Закинул тестовую страницу, картинку, CSS, для теста на сервере

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roman
+ * Date: 14.12.15
+ * Time: 21:44
+ */
+?>
+<html>
+<head>
+    <title>
+        SL4M
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/deerPage.css">
+</head>
+<body>
+<form>
+    <p>Greetings!</p>
+</form>
+</body>
+</html>
+
