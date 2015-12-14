d89a02abf8bd11d9d412a50884dd7d9473d9b478
iSosnitsky <spiritblossom@icloud.com>
2015-12-15 01:57:02 +0500

    Добавлена страница-отвечалка для теста

--- index.php
+++ index.php
@@ -8,14 +8,39 @@
 ?>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>
         SL4M
     </title>
     <link rel="stylesheet" type="text/css" href="css/deerPage.css">
 </head>
 <body>
-<form>
+<form method="post" action="index.php">
     <p>Greetings!</p>
+    <p>Ебошь вопрос:</p>
+    <p><textarea name="question"></textarea></p>
+    <p>Ток шоб да или нет<input type="checkbox" name="simplify"></p>
+    <br>
+    <input type="submit" value="Отвечай ебана">
+    <br>
+    <p>
+    <?
+        $answers = array("да", "нет", "тебя это ебет что ли", "успокойся бля все норм будет чо ты", "толстота ебаная съеби нахуй", "Как бы да, но лучше бы нет", "нихуя", "100%", "я бы тебе сказал шансы, но ты расстроишься", "иди нахуй");
+        if($_POST["question"]!=null){
+            $question = trim($_POST["question"]);
+            if($_POST["simplify"]){
+                echo (rand(1,2)==2) ? "Да" : "Нет";
+            } elseif (substr_count($question, " или ")>0) {
+                if($question[count($question)-1]=='?'){ $question[count($question)]=''; }
+                $dividedQuestion = explode(" или ", $question);
+                $randKey = array_rand($dividedQuestion, 1);
+                echo $dividedQuestion[$randKey[0]];
+            } else {
+                echo $answers[rand(0,9)];
+            }
+        }
+    ?>
+    </p>
 </form>
 </body>
 </html>
