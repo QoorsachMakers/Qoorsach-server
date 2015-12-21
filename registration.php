51608bac62370bad583d74208e3c7ef3597d6197
iSosnitsky <spiritblossom@icloud.com>
2015-12-21 19:26:30 +0500

    Капча теперь работает.

--- registration.php
+++ registration.php
@@ -1,49 +1,44 @@
 <?php
-include_once ('global.php');
-if(isset($_POST['submit'])){
+session_start();
+include_once('global.php');
+if (isset($_POST['submit'])) {
     $err = array();
     $captcha = $_SESSION['code'];
-    if($_POST['captcha_code']!=$captcha){
+    if ($_POST['captcha_code'] != $captcha) {
         $err[] = '– Капча введена неправильно';
-    } else{
-        #проверяем логин
-        if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['user_login'])) #я это честно спиздил
-        {
-            $err[] = "– Логин может состоять только из букв английского алфавита и цифр";
-        }
-
-        if(strlen($_POST['user_login']) < 3 or strlen($_POST['user_login']) > 30)
-        {
-            $err[] = "– Логин должен быть не меньше 3-х символов и не больше 30";
-        }
-
-        if(strlen($_POST['user_password']) < 6 or strlen($_POST['user_password']) > 20)
-        {
-            $err[] = "– Пароль должен быть не меньше 6-и символов и не больше 20";
-        }
-
-        # проверяем, не сущестует ли пользователя с таким именем
-        $query = mysqli_query($databaseLink, "SELECT * FROM Students WHERE Student_Login='".mysqli_real_escape_string($databaseLink, $_POST['user_login'])."'");
-        if(mysqli_num_rows($query) > 0)
-        {
-            $err[] = "– Пользователь с таким логином уже существует в базе данных";
-        }
-
-        $query = mysqli_query($databaseLink, "SELECT * FROM Students WHERE Student_Email='".mysqli_real_escape_string($databaseLink, $_POST['user_email'])."'");
-        if(mysqli_num_rows($query) > 0)
-        {
-            $err[] = "– Пользователь с таким мылом уже существует в базе данных";
-        }
-
-
-        $email = $_POST["user_email"];
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $err[] = "– Некорректный Email";
-        }
+    }
+    #проверяем логин
+    if (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['user_login'])) #я это честно спиздил
+    {
+        $err[] = "– Логин может состоять только из букв английского алфавита и цифр";
     }
 
-    if(count($err) == 0)
-    {
+    if (strlen($_POST['user_login']) < 3 or strlen($_POST['user_login']) > 30) {
+        $err[] = "– Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    if (strlen($_POST['user_password']) < 6 or strlen($_POST['user_password']) > 20) {
+        $err[] = "– Пароль должен быть не меньше 6-и символов и не больше 20";
+    }
+
+    # проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($databaseLink, "SELECT * FROM Students WHERE Student_Login='" . mysqli_real_escape_string($databaseLink, $_POST['user_login']) . "'");
+    if (mysqli_num_rows($query) > 0) {
+        $err[] = "– Пользователь с таким логином уже существует в базе данных";
+    }
+
+    $query = mysqli_query($databaseLink, "SELECT * FROM Students WHERE Student_Email='" . mysqli_real_escape_string($databaseLink, $_POST['user_email']) . "'");
+    if (mysqli_num_rows($query) > 0) {
+        $err[] = "– Пользователь с таким мылом уже существует в базе данных";
+    }
+
+    $email = $_POST["user_email"];
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $err[] = "– Некорректный Email";
+    }
+
+
+    if (count($err) == 0) {
 
         $login = $_POST['user_login'];
         $email = $_POST['user_email'];
@@ -52,23 +47,20 @@ if(isset($_POST['submit'])){
         $hash = md5($password);
 
         $subject = 'Ваша регистрация на моем сайтике';
-        $message = 'Ваш логин:'.$login."\r\n".
-            'Ваш пароль:'.$password;
-        mail($email,$subject,$message);
+        $message = 'Ваш логин:' . $login . "\r\n" .
+            'Ваш пароль:' . $password;
+        mail($email, $subject, $message);
 
-        mysqli_query($databaseLink,"INSERT INTO Students SET Student_Login='".$login."', Student_Password='".$password."', Student_Email='".$email."', Student_Hash='".$hash."'");
+        mysqli_query($databaseLink, "INSERT INTO Students SET Student_Login='" . $login . "', Student_Password='" . $password . "', Student_Email='" . $email . "', Student_Hash='" . $hash . "'");
         print   "<div class='feed'>"
-            ."Успешная регистрация"."<br>".
+            . "Успешная регистрация" . "<br>" .
             "</div><br>";
 
-    }
-    else
-    {
+    } else {
         print "<div class='feed'>";
         print "<b>При регистрации произошли следующие ошибки:</b><br>";
-        foreach($err AS $error)
-        {
-            print $error."<br>";
+        foreach ($err AS $error) {
+            print $error . "<br>";
         }
         print "</div><br>";
     }
@@ -87,11 +79,11 @@ if(isset($_POST['submit'])){
     </p>
     <table>
         <tr>
-            <td>Логин <span style='color: firebrick'>*</span> </td>
+            <td>Логин <span style='color: firebrick'>*</span></td>
             <td><input name="user_login"></td>
         </tr>
         <tr>
-            <td>Пароль <span style='color: firebrick'>*</span> </td>
+            <td>Пароль <span style='color: firebrick'>*</span></td>
             <td><input type="password" name="user_password"></td>
         </tr>
         <tr>
@@ -99,9 +91,9 @@ if(isset($_POST['submit'])){
             <td><input type="email" name="user_email"></td>
         </tr>
     </table>
-    <img class="captcha" src="scripts/captcha.php"/>
-    <p>Введи капчу:<input type="text" name="captcha_code"></p>
-    <input class="btn" name="submit" type="submit"  value="Зарегаться">
+    <img class="captcha" onclick="this.src = 'scripts/captcha.php?' + Math.random();" src="scripts/captcha.php"/>
+    <p>Введи капчу:<br><input type="text" name="captcha_code"></p>
+    <input class="btn" name="submit" type="submit" value="Зарегаться">
 </form>
 </body>
 </html>
--- scripts/captcha.php
+++ scripts/captcha.php
@@ -7,43 +7,43 @@ $fon_let_amount = 30;       //Количество символов на фон�
 $font = "cour.ttf";         //Путь к шрифту
 
 //набор символов
-$letters = array("a","b","c","d","e","f","g");
+$letters = array("a", "b", "c", "d", "e", "f", "g", "s", "n", "z", "y");
 //цвета
-$colors = array("90","110","130","150","170","190","210");
+$colors = array("90", "110", "130", "150", "170", "190", "210");
 
-$src = imagecreatetruecolor($width,$height);    //создаем изображение
-$fon = imagecolorallocate($src,255,255,255);    //создаем фон
-imagefill($src,0,0,$fon);                       //заливаем изображение фоном
+$src = imagecreatetruecolor($width, $height);    //создаем изображение
+$fon = imagecolorallocate($src, 255, 255, 255);    //создаем фон
+imagefill($src, 0, 0, $fon);                       //заливаем изображение фоном
 
-for($i=0;$i < $fon_let_amount;$i++)          //добавляем на фон буковки
+for ($i = 0; $i < $fon_let_amount; $i++)          //добавляем на фон буковки
 {
     //случайный цвет
-    $color = imagecolorallocatealpha($src,rand(0,255),rand(0,255),rand(0,255),100);
+    $color = imagecolorallocatealpha($src, rand(0, 255), rand(0, 255), rand(0, 255), 100);
     //случайный символ
-    $letter = $letters[rand(0,sizeof($letters)-1)];
+    $letter = $letters[rand(0, sizeof($letters) - 1)];
     //случайный размер
-    $size = rand($font_size-2,$font_size+2);
-    imagettftext($src,$size,rand(0,45),
-        rand($width*0.1,$width-$width*0.1),
-        rand($height*0.2,$height),$color,$font,$letter);
+    $size = rand($font_size - 2, $font_size + 2);
+    imagettftext($src, $size, rand(0, 45),
+        rand($width * 0.1, $width - $width * 0.1),
+        rand($height * 0.2, $height), $color, $font, $letter);
 }
 
-for($i=0;$i < $let_amount;$i++)      //то же самое для основных букв
+for ($i = 0; $i < $let_amount; $i++)      //то же самое для основных букв
 {
-    $color = imagecolorallocatealpha($src,$colors[rand(0,sizeof($colors)-1)],
-        $colors[rand(0,sizeof($colors)-1)],
-        $colors[rand(0,sizeof($colors)-1)],rand(20,40));
-    $letter = $letters[rand(0,sizeof($letters)-1)];
-    $size = rand($font_size*2-2,$font_size*2+2);
-    $x = ($i+1)*$font_size + rand(1,5);      //даем каждому символу случайное смещение
-    $y = (($height*2)/3) + rand(0,5);
+    $color = imagecolorallocatealpha($src, $colors[rand(0, sizeof($colors) - 1)],
+        $colors[rand(0, sizeof($colors) - 1)],
+        $colors[rand(0, sizeof($colors) - 1)], rand(20, 40));
+    $letter = $letters[rand(0, sizeof($letters) - 1)];
+    $size = rand($font_size * 2 - 2, $font_size * 2 + 2);
+    $x = ($i + 1) * $font_size + rand(1, 5);      //даем каждому символу случайное смещение
+    $y = (($height * 2) / 3) + rand(0, 5);
     $cod[] = $letter;                        //запоминаем код
-    imagettftext($src,$size,rand(0,15),$x,$y,$color,$font,$letter);
+    imagettftext($src, $size, rand(0, 15), $x, $y, $color, $font, $letter);
 }
 
-$kod = implode("",$cod);                    //переводим код в строку
+$kod = implode("", $cod);                    //переводим код в строку
 session_start();
-$_SESSION['code']=$kod;
-header ("Content-type: image/gif");         //выводим готовую картинку
+$_SESSION['code'] = $kod;
+header("Content-type: image/gif");         //выводим готовую картинку
 imagegif($src);
 ?>
